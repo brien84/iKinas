f792731c62a219855c650816915f04dd28ce635c
brien84 <houseinblues@gmail.com>
2021-07-27 14:46:24 +0300

    Add overlay view

--- Cinema/Views/VenueImageView.swift
+++ Cinema/Views/VenueImageView.swift
@@ -12,6 +12,7 @@ final class VenueImageView: UIImageView {
     var venue: Venue? {
         didSet {
             image = venueImage
+            overlayLabel.text = venue?.rawValue
         }
     }
 
@@ -27,6 +28,74 @@ final class VenueImageView: UIImageView {
             return .multikino
         default:
             return UIImage()
+        }
+    }
+
+    private var showOverlay = false {
+        didSet {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                if self.showOverlay {
+                    self.toggleOverlay()
+                }
+            }
+        }
+    }
+
+    private let overlayLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .preferredFont(forTextStyle: .caption1)
+        label.textColor = .secondaryElement
+        return label
+    }()
+
+    private lazy var overlay: UIView = {
+        let view = UIView()
+        view.alpha = 0
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .secondaryBackground
+        view.layer.cornerRadius = 10
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor.secondaryElement.cgColor
+
+        view.addSubview(overlayLabel)
+
+        NSLayoutConstraint.activate([
+            overlayLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: .padding),
+            overlayLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -.padding),
+            overlayLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: .padding),
+            overlayLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -.padding)
+        ])
+
+        return view
+    }()
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+
+        self.clipsToBounds = false
+        self.isUserInteractionEnabled = true
+        self.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(toggleOverlay)))
+    }
+
+    override func awakeFromNib() {
+        addSubview(overlay)
+
+        self.image = venueImage
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        overlay.frame = overlay.frame.offsetBy(dx: self.bounds.midX - overlay.bounds.midX,
+                                               dy: -overlay.frame.height - .padding)
+    }
+
+    @objc private func toggleOverlay() {
+        showOverlay.toggle()
+
+        UIView.animate(withDuration: 1.0) { [self] in
+            overlay.alpha = showOverlay ? 1 : 0
         }
     }
 }
