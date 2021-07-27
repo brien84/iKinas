eba1d9feb71fb366fd3000d3237c3843bd7580ae
brien84 <houseinblues@gmail.com>
2021-07-27 13:54:54 +0300

    Create `VenueImageView`

---
+++ Cinema/Views/VenueImageView.swift
@@ -0,0 +1,18 @@
+//
+//  VenueImageView.swift
+//  Cinema
+//
+//  Created by Marius on 2021-07-26.
+//  Copyright © 2021 Marius. All rights reserved.
+//
+
+import UIKit
+
+final class VenueImageView: UIImageView {
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+
+    }
+    
+}
