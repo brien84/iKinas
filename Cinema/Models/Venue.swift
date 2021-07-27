ae7e86c5705b597f3001e2588f02d7645e24dbe3
brien84 <houseinblues@gmail.com>
2021-07-27 13:56:43 +0300

    Create `Venue` model

---
+++ Cinema/Models/Venue.swift
@@ -0,0 +1,17 @@
+//
+//  Venue.swift
+//  Cinema
+//
+//  Created by Marius on 2021-07-27.
+//  Copyright © 2021 Marius. All rights reserved.
+//
+
+import Foundation
+
+enum Venue: String, Codable {
+    case cinamon = "Cinamon"
+    case forum = "Forum Cinemas"
+    case forumAkropolis = "Forum Cinemas Akropolis"
+    case forumVingis = "Forum Cinemas Vingis"
+    case multikino = "Multikino"
+}
