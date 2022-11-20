cd3d4da715558ce32fe490f025bb2486773021fb
brien84 <houseinblues@gmail.com>
2022-11-20 18:49:12 +0200

    Add `VersionVerifier` tests

--- CinemaTests/Models Tests/VersionVerifierTests.swift
+++ CinemaTests/Models Tests/VersionVerifierTests.swift
@@ -7,36 +7,90 @@
 //
 
 import XCTest
+@testable import iKinas
 
+// swiftlint:disable force_try
 final class VersionVerifierTests: XCTestCase {
+    typealias Error = VersionVerifier.VersionError
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var sut: VersionVerifier!
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    override func setUp() {
+        sut = VersionVerifier()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+        sut = nil
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
+    func testVerifyingLowerThanSystemVersion() {
+        let testVersion = Double(1.0)
+        let session = URLSession.makeMockSession(with: try! JSONEncoder().encode(testVersion))
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let expectation = expectation(description: "Waiting for completion handler.")
+        var result: Result<Void, Error>?
+
+        sut.verifyVersion(using: session) {
+            result = $0
+            expectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 1)
+
+        switch result {
+        case .success:
+            XCTAssert(true)
+        case .failure:
+            XCTFail("Verification should succeed!")
+        case .none:
+            XCTFail("Result should have a value!")
+        }
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
+    func testVerifyingHigherThanSystemVersion() {
+        let testVersion = Double(999.0)
+        let session = URLSession.makeMockSession(with: try! JSONEncoder().encode(testVersion))
+
+        let expectation = expectation(description: "Waiting for completion handler.")
+        var result: Result<Void, Error>?
+
+        sut.verifyVersion(using: session) {
+            result = $0
+            expectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 1)
+
+        switch result {
+        case .success:
+            XCTFail("Verification should fail!")
+        case .failure(let error):
+            XCTAssertEqual(error, Error.requiresUpdate)
+        case .none:
+            XCTFail("Result should have a value!")
+        }
+    }
+
+    func testNetworkFailureWhileTryingToVerify() {
+        let session = URLSession.makeMockSession(with: nil)
+
+        let expectation = expectation(description: "Waiting for completion handler.")
+        var result: Result<Void, Error>?
+
+        sut.verifyVersion(using: session) {
+            result = $0
+            expectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 1)
+
+        switch result {
+        case .success:
+            XCTFail("Verification should fail!")
+        case .failure(let error):
+            XCTAssertEqual(error, Error.verificationFailure)
+        case .none:
+            XCTFail("Result should have a value!")
         }
     }
 }
