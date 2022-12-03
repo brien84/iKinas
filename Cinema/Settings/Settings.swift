49a462251546479f05d90639cc5137f67a7ac56f
brien84 <houseinblues@gmail.com>
2022-12-03 19:43:37 +0200

    Create `Settings` interface

---
+++ Cinema/Settings/Settings.swift
@@ -0,0 +1,23 @@
+//
+//  Settings.swift
+//  Cinema
+//
+//  Created by Marius on 2022-12-03.
+//  Copyright © 2022 Marius. All rights reserved.
+//
+
+import ComposableArchitecture
+
+struct Settings: ReducerProtocol {
+    struct State: Equatable {
+
+    }
+
+    enum Action: Equatable {
+
+    }
+
+    func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
+
+    }
+}
