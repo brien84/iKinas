d962650acae648c2a63279dc35907665981cafcb
brien84 <houseinblues@gmail.com>
2022-12-04 00:19:58 +0200

    Create `SettingsTests`

---
+++ CinemaTests/SettingsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsTests.swift
+//  CinemaTests
+//
+//  Created by Marius on 2022-12-04.
+//  Copyright © 2022 Marius. All rights reserved.
+//
+
+import ComposableArchitecture
+import XCTest
+@testable import iKinas
+
+@MainActor
+final class SettingsTests: XCTestCase {
+
+
+}
