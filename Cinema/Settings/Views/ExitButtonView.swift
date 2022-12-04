3db6b652737150f13d94e6580b7a7bbe0d16b252
brien84 <houseinblues@gmail.com>
2022-12-04 12:21:39 +0200

    Create `ExitButtonView`

---
+++ Cinema/Settings/Views/ExitButtonView.swift
@@ -0,0 +1,75 @@
+//
+//  ExitButtonView.swift
+//  Cinema
+//
+//  Created by Marius on 2022-12-04.
+//  Copyright © 2022 Marius. All rights reserved.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+private extension ExitButtonView {
+    struct State: Equatable {
+
+    }
+
+    enum Action: Equatable {
+        case saveSettings
+    }
+}
+
+private extension Settings.State {
+    var state: ExitButtonView.State {
+        .init()
+    }
+}
+
+private extension ExitButtonView.Action {
+    var action: Settings.Action {
+        switch self {
+        case .saveSettings:
+            return .saveSettings
+        }
+    }
+}
+
+struct ExitButtonView: View {
+    @Environment(\.presentationMode) var presentationMode
+
+    let store: StoreOf<Settings>
+
+    var body: some View {
+        WithViewStore(store, observe: \.state, send: \Action.action) { viewStore in
+            Button(
+                action: {
+                    viewStore.send(.saveSettings)
+                    DispatchQueue.main.async {
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                },
+                label: {
+                    Image(systemName: "xmark")
+                        .resizable()
+                        .frame(width: 12, height: 12)
+                        .padding(24)
+                        .font(Font.title3.weight(.bold))
+                        .foregroundColor(Color(.secondaryElement))
+                        .background(
+                            Circle()
+                                .fill(Color(.primaryBackground))
+                                .frame(width: 30)
+                        )
+                }
+            )
+        }
+    }
+}
+
+// MARK: - Previews
+
+struct ExitButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExitButtonView(store: Store(initialState: Settings.State(), reducer: Settings()))
+    }
+}
