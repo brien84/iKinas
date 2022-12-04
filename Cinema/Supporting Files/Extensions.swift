59da4c678e76650caab34918591165afd23187f2
brien84 <houseinblues@gmail.com>
2022-12-04 12:07:43 +0200

    Add `hidden(_)` `View` extension method

--- Cinema/Supporting Files/Extensions.swift
+++ Cinema/Supporting Files/Extensions.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019 Marius. All rights reserved.
 //
 
+import SwiftUI
 import UIKit
 
 extension Date {
@@ -99,5 +100,17 @@ extension UserDefaults {
         } else {
             return City.vilnius
         }
+    }
+}
+
+extension View {
+    /// Dinamically hides and disables view.
+    ///
+    /// Since iOS15 the behaviour of `XCUIElement` `isHittable` property
+    /// has changed in a way that it no longer returns true if `View` opacity is zero.
+    /// As a workaround this function hides and also disables the `View`
+    /// therefore we can now assert on `XCUIElement` `isEnabled` property.
+    func hidden(_ hidden: Bool) -> some View {
+        opacity(hidden ? 0 : 1).disabled(hidden)
     }
 }
