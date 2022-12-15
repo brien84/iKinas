38f3f18edeb3ea74f3d81490f59689706bfdcd52
brien84 <houseinblues@gmail.com>
2022-12-15 17:57:23 +0200

    Refactor `DateSelector` initialisation

--- Cinema/DateSelector/DateSelector.swift
+++ Cinema/DateSelector/DateSelector.swift
@@ -10,21 +10,29 @@ import ComposableArchitecture
 import Foundation
 
 struct DateSelector: ReducerProtocol {
+    
     struct State: Equatable {
-        let dates: [Date]
-        var selected: Date
+        let today: Date
+        let restOfTheWeek: [Date]
+        var selectedDate: Date
+
+        var isTodaySelected: Bool {
+            today == selectedDate
+        }
 
         init() {
-            var dates = [Date]()
-            let now = Date()
-            for index in 0..<8 {
-                guard let date = Calendar.current.date(byAdding: .day, value: index, to: now)
+            self.today = Date()
+            self.selectedDate = self.today
+
+            var restOfTheWeek = [Date]()
+
+            for index in 0..<7 {
+                guard let date = Calendar.current.date(byAdding: .day, value: index, to: self.today)
                 else { fatalError("Date generation failed!") }
-                dates.append(date)
+                restOfTheWeek.append(date)
             }
 
-            self.dates = dates
-            self.selected = dates[0]
+            self.restOfTheWeek = restOfTheWeek
         }
     }
 
@@ -35,7 +43,7 @@ struct DateSelector: ReducerProtocol {
     func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
         switch action {
         case .didSelect(date: let date):
-            state.selected = date
+            state.selectedDate = date
             return .none
         }
     }
