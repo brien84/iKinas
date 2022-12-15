5e439bbbd61ee8a2c56cbb7c9bc07642c8f77daf
brien84 <houseinblues@gmail.com>
2022-12-15 20:12:36 +0200

    Implement `DateSelectorView` view

--- Cinema/DateSelector/DateSelectorView.swift
+++ Cinema/DateSelector/DateSelectorView.swift
@@ -13,11 +13,63 @@ struct DateSelectorView: View {
     let store: StoreOf<DateSelector>
 
     var body: some View {
-        WithViewStore(store) { _ in
+        WithViewStore(store) { viewStore in
             ZStack {
+                HStack(spacing: .zero) {
+                    Button {
+                        viewStore.send(.didSelect(date: viewStore.today))
+                    } label: {
+                        Image(systemName: "house")
+                            .foregroundColor(viewStore.isTodaySelected ? .tertiaryElement : .primaryElement)
+                            .padding(.horizontal)
+                    }
 
+                    Divider()
+
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        LazyHStack(spacing: 24) {
+                            ForEach(viewStore.restOfTheWeek, id: \.self) { date in
+                                Button {
+                                    viewStore.send(.didSelect(date: date))
+                                } label: {
+                                    VStack {
+                                        Text(date.shortWeekdayFormat)
+                                            .font(.caption.weight(.bold))
+                                            .foregroundColor(date == viewStore.selectedDate ? .tertiaryElement : .primaryElement)
+
+                                        Text(date.shortDateFormat)
+                                            .font(.caption2)
+                                            .foregroundColor(date == viewStore.selectedDate ? .tertiaryElement : .secondaryElement)
+                                    }
+                                }
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
+                }
             }
+            .padding(.vertical)
+            .fixedSize(horizontal: false, vertical: true)
+            .background(Color.secondaryBackground)
         }
+    }
+}
+
+private extension Date {
+    var shortWeekdayFormat: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "lt")
+        dateFormatter.shortWeekdaySymbols = ["SEK", "PIR", "ANT", "TRE", "KET", "PEN", "ŠEŠ"]
+        dateFormatter.dateFormat = "EEE"
+        return dateFormatter.string(from: self)
+    }
+
+    var shortDateFormat: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "lt")
+        dateFormatter.shortMonthSymbols = ["Sau", "Vas", "Kov", "Bal", "Geg", "Bir", "Lie", "Rgp", "Rgs", "Spa", "Lap", "Gru"]
+        dateFormatter.dateFormat = "MMM d"
+        return dateFormatter.string(from: self)
     }
 }
 
