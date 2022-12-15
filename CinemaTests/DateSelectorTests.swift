5452a2a2bb9c45e64cd057f4feb77f2d750b698d
brien84 <houseinblues@gmail.com>
2022-12-15 17:00:04 +0200

    Create `DateSelectorTests`

---
+++ CinemaTests/DateSelectorTests.swift
@@ -0,0 +1,20 @@
+//
+//  DateSelectorTests.swift
+//  CinemaTests
+//
+//  Created by Marius on 2022-12-15.
+//  Copyright © 2022 Marius. All rights reserved.
+//
+
+import ComposableArchitecture
+import XCTest
+@testable import iKinas
+
+@MainActor
+final class DateSelectorTests: XCTestCase {
+
+    func test0() async {
+        XCTAssert(true)
+    }
+
+}
