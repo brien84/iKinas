fe2be345b96607618f4769eeb8f9dc51a488c638
brien84 <houseinblues@gmail.com>
2022-12-15 17:03:51 +0200

    Create `MainViewController`

---
+++ Cinema/View Controllers/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  Cinema
+//
+//  Created by Marius on 2022-12-15.
+//  Copyright © 2022 Marius. All rights reserved.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
