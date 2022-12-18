34963a8223111c75d3d082518d1ccb0b34fb01ea
brien84 <houseinblues@gmail.com>
2022-12-18 11:26:26 +0200

    Create `MovieListTests`

---
+++ CinemaTests/MovieListTests.swift
@@ -0,0 +1,20 @@
+//
+//  MovieListTests.swift
+//  CinemaTests
+//
+//  Created by Marius on 2022-12-18.
+//  Copyright © 2022 Marius. All rights reserved.
+//
+
+import ComposableArchitecture
+import XCTest
+@testable import iKinas
+
+@MainActor
+final class MovieListTests: XCTestCase {
+
+    func test() async {
+        XCTAssertTrue(true)
+    }
+
+}
