f90847fb2bf265b5bf915674e99475589505dc10
brien84 <houseinblues@gmail.com>
2022-12-18 14:39:14 +0200

    Implement image fetching

--- Cinema/NetworkImage/NetworkImage.swift
+++ Cinema/NetworkImage/NetworkImage.swift
@@ -8,21 +8,41 @@
 
 import ComposableArchitecture
 import SwiftUI
+import Foundation
 
 struct NetworkImage: ReducerProtocol {
+    static let defaultImage = UIImage(named: "defaultPoster")
 
     struct State: Equatable {
-        var url: URL
         var image: UIImage?
+        var isFetching = false
+        var url: URL
     }
 
     enum Action: Equatable {
-        case none
+        case fetch
+        case imageClient(Result<UIImage, ImageClient.Failure>)
     }
+
+    @Dependency(\.imageClient) var imageClient
+    @Dependency(\.mainQueue) var mainQueue
 
     func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
         switch action {
-        case .none:
+        case .fetch:
+            state.isFetching = true
+            return imageClient.fetch(state.url)
+                .receive(on: mainQueue)
+                .catchToEffect(Action.imageClient)
+
+        case .imageClient(let result):
+            state.isFetching = false
+            switch result {
+            case .success(let image):
+                state.image = image
+            case .failure:
+                state.image = Self.defaultImage
+            }
             return .none
         }
     }
