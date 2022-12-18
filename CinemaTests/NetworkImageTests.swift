6b4ddb1161db6209aafc420c24b71cbed9a37adb
brien84 <houseinblues@gmail.com>
2022-12-18 11:27:46 +0200

    Create `NetworkImageTests`

---
+++ CinemaTests/NetworkImageTests.swift
@@ -0,0 +1,20 @@
+//
+//  NetworkImageTests.swift
+//  CinemaTests
+//
+//  Created by Marius on 2022-12-18.
+//  Copyright © 2022 Marius. All rights reserved.
+//
+
+import ComposableArchitecture
+import XCTest
+@testable import iKinas
+
+@MainActor
+final class NetworkImageTests: XCTestCase {
+
+    func test() async {
+        XCTAssertTrue(true)
+    }
+
+}
