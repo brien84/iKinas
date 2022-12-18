afe4891a615f7699643fc8e010df30e7020ae91d
brien84 <houseinblues@gmail.com>
2022-12-18 19:09:28 +0200

    Implement `MovieItemView`

--- Cinema/MovieItem/MovieItemView.swift
+++ Cinema/MovieItem/MovieItemView.swift
@@ -12,42 +12,74 @@ import SwiftUI
 struct MovieItemView: View {
     let store: StoreOf<MovieItem>
 
+    @State private var isBeingPressed = false
+
     var body: some View {
         WithViewStore(store) { viewStore in
             ZStack {
-                RoundedRectangle(cornerRadius: 20, style: .continuous)
-                    .foregroundColor(.green)
+                Color.primaryBackground
+
+                NetworkImageViewV2(store: store.scope(
+                    state: \.networkImage,
+                    action: MovieItem.Action.networkImage
+                ))
+                .aspectRatio(.imageAspectRatio, contentMode: .fit)
 
                 VStack {
                     Spacer()
 
-                    LazyVStack(alignment: .leading) {
+                    ZStack(alignment: .leading) {
                         Text(viewStore.movie.title)
-                            .lineLimit(1)
                             .font(.headline)
-                            .foregroundColor(.white)
 
-                        Text(viewStore.movie.originalTitle)
-                            .lineLimit(1)
-                            .font(.subheadline)
-                            .foregroundColor(.white)
+                        Text(String(repeating: "Placeholder", count: 10))
+                            .font(.headline)
+                            .hidden()
                     }
+                    .lineLimit(2)
+                    .foregroundColor(.primaryElement)
                     .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
                 }
             }
+            .clipShape(RoundedRectangle(cornerRadius: .cornerRadius, style: .continuous))
+            .opacity(isBeingPressed ? CGFloat.longPressOpacity : 1)
+            .scaleEffect(isBeingPressed ? .longPressScaleEffect : 1)
+            .onTapGesture {
+                viewStore.send(.didSelectMovie(viewStore.movie))
+            }
+            .onLongPressGesture(perform: { }, onPressingChanged: { isPressing in
+                withAnimation(.longPressSpring) {
+                    isBeingPressed = isPressing
+                }
+            })
         }
     }
 }
 
-struct MovieItemView_Previews: PreviewProvider {
-    static let movie = Movie.create(title: "Movie Title", originalTitle: "Movie Title But In Foreign Language")
+private extension Animation {
+    static let longPressSpring = Animation.spring(
+        response: 0.5,
+        dampingFraction: 0.5
+    )
+}
 
+private extension CGFloat {
+    static let cornerRadius: CGFloat = 10
+    static let imageAspectRatio: CGFloat = 2/3
+    static let longPressOpacity: CGFloat = 0.95
+    static let longPressScaleEffect: CGFloat = 0.95
+
+}
+
+struct MovieItemView_Previews: PreviewProvider {
     static let store = Store(
-        initialState: MovieItem.State(id: UUID(), movie: movie),
+        initialState: MovieItem.State(id: UUID(), movie: Movie()),
         reducer: MovieItem()
     )
 
     static var previews: some View {
         MovieItemView(store: store)
+            .frame(width: 200, height: 300)
     }
 }
