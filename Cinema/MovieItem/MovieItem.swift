57538bd4e7ad7f48d2a4ca660590b54b53613a87
brien84 <houseinblues@gmail.com>
2022-12-18 18:01:54 +0200

    Implement `NetworkImage` functionallity

--- Cinema/MovieItem/MovieItem.swift
+++ Cinema/MovieItem/MovieItem.swift
@@ -14,21 +14,30 @@ struct MovieItem: ReducerProtocol {
     struct State: Equatable, Identifiable {
         let id: UUID
         let movie: Movie
+        var networkImage: NetworkImage.State
 
         init(id: UUID, movie: Movie) {
             self.id = id
             self.movie = movie
+            self.networkImage = NetworkImage.State(url: movie.poster)
         }
     }
 
     enum Action: Equatable {
         case didSelectMovie(Movie)
+        case networkImage(NetworkImage.Action)
     }
 
     var body: some ReducerProtocol<State, Action> {
+        Scope(state: \.networkImage, action: /Action.networkImage) {
+            NetworkImage()
+        }
+
         Reduce { _, action in
             switch action {
             case .didSelectMovie:
+                return .none
+            case .networkImage:
                 return .none
             }
         }
