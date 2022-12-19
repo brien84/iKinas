bf79ebda3ea447ab363749b1176309b4e0588515
brien84 <houseinblues@gmail.com>
2022-12-19 11:11:06 +0200

    Create `ScheduleView`

---
+++ Cinema/Schedule/ScheduleView.swift
@@ -0,0 +1,32 @@
+//
+//  ScheduleView.swift
+//  Cinema
+//
+//  Created by Marius on 2022-12-19.
+//  Copyright © 2022 Marius. All rights reserved.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+struct ScheduleView: View {
+    let store: StoreOf<Schedule>
+
+    var body: some View {
+        WithViewStore(store) { _ in
+            ZStack {
+                Color.primaryBackground
+
+            }
+        }
+    }
+}
+
+struct ScheduleView_Previews: PreviewProvider {
+    static let store = Store(initialState: Schedule.State(), reducer: Schedule())
+
+    static var previews: some View {
+        ScheduleView(store: store)
+            .preferredColorScheme(.dark)
+    }
+}
