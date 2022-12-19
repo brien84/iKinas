ec2a7027869ed582933793596d242709320de1a4
brien84 <houseinblues@gmail.com>
2022-12-19 19:25:44 +0200

    Add `testUpdatingShowingItems()` test case

--- CinemaTests/ShowingListTests.swift
+++ CinemaTests/ShowingListTests.swift
@@ -13,8 +13,21 @@ import XCTest
 @MainActor
 final class ShowingListTests: XCTestCase {
 
-    func test() async {
-        XCTAssertTrue(true)
+    func testUpdatingShowingItems() async {
+        let store = TestStore(
+            initialState: ShowingList.State(),
+            reducer: ShowingList()
+        )
+
+        let uuid = UUID(uuidString: "00000000-0000-0000-0000-000000000000")!
+        store.dependencies.uuid = .constant(uuid)
+
+        let movie = Movie()
+        let showing = Showing(parentMovie: movie)
+
+        await store.send(.update(showings: [showing])) {
+            $0.showingItems = [ShowingItem.State(id: uuid, showing: showing)]
+        }
     }
 
 }
