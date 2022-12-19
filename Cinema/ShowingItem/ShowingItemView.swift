2647dc19b433edb721578385d04b2f5b9ed6d082
brien84 <houseinblues@gmail.com>
2022-12-19 18:14:42 +0200

    Add `NetworkImageViewV2` view

--- Cinema/ShowingItem/ShowingItemView.swift
+++ Cinema/ShowingItem/ShowingItemView.swift
@@ -18,44 +18,63 @@ struct ShowingItemView: View {
                 Color.primaryBackground
 
                 HStack {
+                    NetworkImageViewV2(store: store.scope(
+                        state: \.networkImage,
+                        action: ShowingItem.Action.networkImage
+                    ))
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: .width, height: .height)
+                    .clipShape(RoundedRectangle(cornerRadius: .cornerRadius))
+
                     VStack {
-                        Text(viewStore.showing.parentMovie?.title ?? "")
+                        #warning("Fix force unwrap!")
+                        Text(viewStore.showing.parentMovie!.title)
                             .font(.headline)
                             .lineLimit(2)
-                            .foregroundColor(.white)
+                            .foregroundColor(.primaryElement)
                             .frame(maxWidth: .infinity, alignment: .leading)
 
-                        Text(viewStore.showing.parentMovie?.originalTitle ?? "")
+                        #warning("Fix force unwrap!")
+                        Text(viewStore.showing.parentMovie!.originalTitle)
                             .font(.subheadline)
                             .lineLimit(1)
-                            .foregroundColor(.gray)
+                            .foregroundColor(.secondaryElement)
                             .frame(maxWidth: .infinity, alignment: .leading)
                     }
-                    .frame(maxWidth: .infinity)
 
                     VStack(alignment: .trailing) {
                         Text(viewStore.showing.date.asString(.timeOfDay))
+                            .foregroundColor(.primaryElement)
                             .font(.title3)
 
                         Image(viewStore.showing.venue.rawValue)
                     }
 
                 }
-                .padding(8)
+                .padding(.padding)
 
             }
         }
     }
 }
 
+private extension CGFloat {
+    static let cornerRadius: CGFloat = 10
+    static let height: CGFloat = 75
+    static let width: CGFloat = 75
+    static let padding: CGFloat = 8
+}
+
 struct ShowingItemView_Previews: PreviewProvider {
+    static let movie = Movie()
+
     static let store = Store(
-        initialState: ShowingItem.State(id: UUID(), showing: Showing(parentMovie: Movie())),
+        initialState: ShowingItem.State(id: UUID(), showing: Showing(parentMovie: movie)),
         reducer: ShowingItem()
     )
 
     static var previews: some View {
         ShowingItemView(store: store)
-            .frame(height: 75)
+            .fixedSize(horizontal: false, vertical: true)
     }
 }
