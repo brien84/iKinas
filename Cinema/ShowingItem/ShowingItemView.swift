f15a52fc36e72b33ff48556e8535a3780dcf7c73
brien84 <houseinblues@gmail.com>
2022-12-19 14:58:45 +0200

    Create `ShowingItemView`

---
+++ Cinema/ShowingItem/ShowingItemView.swift
@@ -0,0 +1,61 @@
+//
+//  ShowingItemView.swift
+//  Cinema
+//
+//  Created by Marius on 2022-12-19.
+//  Copyright © 2022 Marius. All rights reserved.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+struct ShowingItemView: View {
+    let store: StoreOf<ShowingItem>
+
+    var body: some View {
+        WithViewStore(store) { viewStore in
+            ZStack {
+                Color.primaryBackground
+
+                HStack {
+                    VStack {
+                        Text(viewStore.showing.parentMovie?.title ?? "")
+                            .font(.headline)
+                            .lineLimit(2)
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+
+                        Text(viewStore.showing.parentMovie?.originalTitle ?? "")
+                            .font(.subheadline)
+                            .lineLimit(1)
+                            .foregroundColor(.gray)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                    .frame(maxWidth: .infinity)
+
+                    VStack(alignment: .trailing) {
+                        Text(viewStore.showing.date.asString(.timeOfDay))
+                            .font(.title3)
+
+                        Image(viewStore.showing.venue.rawValue)
+                    }
+
+                }
+                .padding(8)
+
+            }
+        }
+    }
+}
+
+struct ShowingItemView_Previews: PreviewProvider {
+    static let store = Store(
+        initialState: ShowingItem.State(id: UUID(), showing: Showing(parentMovie: Movie())),
+        reducer: ShowingItem()
+    )
+
+    static var previews: some View {
+        ShowingItemView(store: store)
+            .frame(height: 75)
+    }
+}
