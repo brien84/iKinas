95a169f2b6c3890021310ef0c99a13bcc191ac76
brien84 <houseinblues@gmail.com>
2022-12-19 19:28:33 +0200

    Integrate `ShowingItemView` views

--- Cinema/ShowingList/ShowingListView.swift
+++ Cinema/ShowingList/ShowingListView.swift
@@ -14,19 +14,38 @@ struct ShowingListView: View {
 
     var body: some View {
         WithViewStore(store) { _ in
-            LazyVStack {
-
+            LazyVStack(spacing: .verticalSpacing) {
+                ForEachStore(
+                    store.scope(state: \.showingItems, action: ShowingList.Action.showingItem(id:action:))
+                ) {
+                    ShowingItemView(store: $0)
+                        .padding(.horizontal, .horizontalPadding)
+                }
             }
         }
 
     }
 }
 
+private extension CGFloat {
+    static let horizontalPadding: CGFloat = 8
+    static let verticalSpacing: CGFloat = 8
+}
+
 struct ShowingListView_Previews: PreviewProvider {
-    static let store = Store(initialState: ShowingList.State(), reducer: ShowingList())
+    static let movie = Movie()
+    static let showings = Array(repeating: Showing(parentMovie: movie), count: 5)
+
+    static let store = Store(initialState: ShowingList.State(showings: showings), reducer: ShowingList())
 
     static var previews: some View {
         ShowingListView(store: store)
             .preferredColorScheme(.dark)
+    }
+}
+
+private extension ShowingList.State {
+    init(showings: [Showing]) {
+        self.showingItems = IdentifiedArray(uniqueElements: showings.map { ShowingItem.State(id: UUID(), showing: $0) })
     }
 }
