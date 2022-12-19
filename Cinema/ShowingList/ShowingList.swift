b7660db8d2f2517d200f7dcff70eb7de7edc1497
brien84 <houseinblues@gmail.com>
2022-12-19 19:25:10 +0200

    Integrate `ShowingItem.State`

--- Cinema/ShowingList/ShowingList.swift
+++ Cinema/ShowingList/ShowingList.swift
@@ -7,24 +7,46 @@
 //
 
 import ComposableArchitecture
-import SwiftUI
+import Foundation
 
 struct ShowingList: ReducerProtocol {
 
     struct State: Equatable {
-
+        var showingItems: IdentifiedArrayOf<ShowingItem.State> = []
+        var selectedShowing: Showing?
     }
 
     enum Action: Equatable {
-        case none
+        case didDeselectShowing
+        case showingItem(id: ShowingItem.State.ID, action: ShowingItem.Action)
+        case update(showings: [Showing])
     }
 
+    @Dependency(\.uuid) var uuid
+
     var body: some ReducerProtocol<State, Action> {
-        Reduce { _, action in
+        Reduce { state, action in
             switch action {
-            case .none:
+
+            case .didDeselectShowing:
+                state.selectedShowing = nil
                 return .none
+
+            case .showingItem(id: _, action: .didSelectShowing(let showing)):
+                state.selectedShowing = showing
+                return .none
+
+            case .showingItem:
+                return .none
+
+            case .update(showings: let showings):
+                state.showingItems = IdentifiedArray(uniqueElements: showings.map { ShowingItem.State(id: uuid(), showing: $0) })
+                return .none
+
             }
+        }
+        .forEach(\.showingItems, action: /Action.showingItem(id:action:)) {
+            ShowingItem()
         }
     }
 
