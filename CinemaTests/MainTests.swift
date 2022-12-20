9f08a81ddaadb2eaf380ef168544bc1575bbf719
brien84 <houseinblues@gmail.com>
2022-12-20 15:45:32 +0200

    Create `MainTests`

---
+++ CinemaTests/MainTests.swift
@@ -0,0 +1,20 @@
+//
+//  MainTests.swift
+//  CinemaTests
+//
+//  Created by Marius on 2022-12-20.
+//  Copyright © 2022 Marius. All rights reserved.
+//
+
+import ComposableArchitecture
+import XCTest
+@testable import iKinas
+
+@MainActor
+final class MainTests: XCTestCase {
+
+    func test() async {
+        XCTAssertTrue(true)
+    }
+
+}
