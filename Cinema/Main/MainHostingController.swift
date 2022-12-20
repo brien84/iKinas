9db686ba0b4a9bf465a702adfb4ed7e39b34a9fd
brien84 <houseinblues@gmail.com>
2022-12-20 15:43:54 +0200

    Create `MainHostingController`

---
+++ Cinema/Main/MainHostingController.swift
@@ -0,0 +1,24 @@
+//
+//  MainHostingController.swift
+//  Cinema
+//
+//  Created by Marius on 2022-12-20.
+//  Copyright © 2022 Marius. All rights reserved.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+final class MainHostingController: UIHostingController<MainView> {
+    private let store: StoreOf<Main>
+
+    required init?(coder aDecoder: NSCoder) {
+        let store = Store(initialState: Main.State(), reducer: Main())
+        self.store = store
+
+        super.init(
+            coder: aDecoder,
+            rootView: MainView(store: store)
+        )
+    }
+}
