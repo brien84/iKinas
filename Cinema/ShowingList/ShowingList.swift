e67aeac2607dc86d57847819c44fc95c46e22b84
brien84 <houseinblues@gmail.com>
2022-12-21 19:05:42 +0200

    Remove redundant actions

--- Cinema/ShowingList/ShowingList.swift
+++ Cinema/ShowingList/ShowingList.swift
@@ -13,36 +13,17 @@ struct ShowingList: ReducerProtocol {
 
     struct State: Equatable {
         var showingItems: IdentifiedArrayOf<ShowingItem.State> = []
-        var selectedShowing: Showing?
     }
 
     enum Action: Equatable {
-        case didDeselectShowing
         case showingItem(id: ShowingItem.State.ID, action: ShowingItem.Action)
-        case update(showings: [Showing])
     }
 
-    @Dependency(\.uuid) var uuid
-
     var body: some ReducerProtocol<State, Action> {
-        Reduce { state, action in
+        Reduce { _, action in
             switch action {
-
-            case .didDeselectShowing:
-                state.selectedShowing = nil
-                return .none
-
-            case .showingItem(id: _, action: .didSelectShowing(let showing)):
-                state.selectedShowing = showing
-                return .none
-
             case .showingItem:
                 return .none
-
-            case .update(showings: let showings):
-                state.showingItems = IdentifiedArray(uniqueElements: showings.map { ShowingItem.State(id: uuid(), showing: $0) })
-                return .none
-
             }
         }
         .forEach(\.showingItems, action: /Action.showingItem(id:action:)) {
