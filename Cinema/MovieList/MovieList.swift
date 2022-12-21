c1e000cb7532e4d9b5142d10079019a385856878
brien84 <houseinblues@gmail.com>
2022-12-21 19:15:48 +0200

    Remove redundant actions

--- Cinema/MovieList/MovieList.swift
+++ Cinema/MovieList/MovieList.swift
@@ -13,38 +13,18 @@ struct MovieList: ReducerProtocol {
 
     struct State: Equatable {
         var movieItems: IdentifiedArrayOf<MovieItem.State> = []
-        var selectedMovie: Movie?
-
         var requiresScrollToTop = false
     }
 
     enum Action: Equatable {
-        case didDeselectMovie
         case movieItem(id: MovieItem.State.ID, action: MovieItem.Action)
-        case update(movies: [Movie])
     }
 
-    @Dependency(\.uuid) var uuid
-
     var body: some ReducerProtocol<State, Action> {
-        Reduce { state, action in
+        Reduce { _, action in
             switch action {
-
-            case .didDeselectMovie:
-                state.selectedMovie = nil
-                return .none
-
-            case .movieItem(id: _, action: .didSelectMovie(let movie)):
-                state.selectedMovie = movie
-                return .none
-
             case .movieItem:
                 return .none
-
-            case .update(movies: let movies):
-                state.movieItems = IdentifiedArray(uniqueElements: movies.map { MovieItem.State(id: uuid(), movie: $0) })
-                return .none
-
             }
         }
         .forEach(\.movieItems, action: /Action.movieItem(id:action:)) {
--- CinemaTests/MovieListTests.swift
+++ CinemaTests/MovieListTests.swift
@@ -13,44 +13,4 @@ import XCTest
 @MainActor
 final class MovieListTests: XCTestCase {
 
-    func testUpdatingMovieItems() async {
-        let store = TestStore(
-            initialState: MovieList.State(),
-            reducer: MovieList()
-        )
-
-        let uuid = UUID(uuidString: "00000000-0000-0000-0000-000000000000")!
-        store.dependencies.uuid = .constant(uuid)
-
-        let movie = Movie()
-
-        await store.send(.update(movies: [movie])) {
-            $0.movieItems = [MovieItem.State(id: uuid, movie: movie)]
-        }
-    }
-
-    func testSelectingAndDeselectingMovie() async {
-        let store = TestStore(
-            initialState: MovieList.State(),
-            reducer: MovieList()
-        )
-
-        let uuid = UUID(uuidString: "00000000-0000-0000-0000-000000000000")!
-        store.dependencies.uuid = .constant(uuid)
-
-        let movie = Movie()
-
-        await store.send(.update(movies: [movie])) {
-            $0.movieItems = [MovieItem.State(id: uuid, movie: movie)]
-        }
-
-        await store.send(.movieItem(id: uuid, action: .didSelectMovie(movie))) {
-            $0.selectedMovie = movie
-        }
-
-        await store.send(.didDeselectMovie) {
-            $0.selectedMovie = nil
-        }
-    }
-
 }
