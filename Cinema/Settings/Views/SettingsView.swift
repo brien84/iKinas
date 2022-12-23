4da54ab263e01871de31f87385fd87db3c519f3c
brien84 <houseinblues@gmail.com>
2022-12-23 18:50:08 +0200

    Delete `SettingsViewHost` `UIHostingController`

--- Cinema/Settings/Views/SettingsView.swift
+++ Cinema/Settings/Views/SettingsView.swift
@@ -9,22 +9,6 @@
 import ComposableArchitecture
 import SwiftUI
 
-final class SettingsViewHost: UIHostingController<SettingsView> {
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-
-        DispatchQueue.main.async {
-            self.navigationController?.isNavigationBarHidden = true
-        }
-    }
-
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-
-        self.navigationController?.isNavigationBarHidden = false
-    }
-}
-
 private extension SettingsView {
     struct State: Equatable {
         var selectedCity: City
