d9932125b165ae23cb05cf7fe3b1d2fcb988c2ec
brien84 <houseinblues@gmail.com>
2022-12-25 22:53:54 +0200

    Create `LoadingViewV2` view

---
+++ Cinema/Main/LoadingViewV2.swift
@@ -0,0 +1,38 @@
+//
+//  LoadingViewV2.swift
+//  Cinema
+//
+//  Created by Marius on 2022-12-25.
+//  Copyright © 2022 Marius. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoadingViewV2: View {
+    @State private var image = 0
+
+    private let timer = Timer.publish(every: 0.03, on: .main, in: .common).autoconnect()
+
+    var body: some View {
+        ZStack {
+            Color.primaryBackground
+                .ignoresSafeArea()
+
+            Image("LoadingAnimation/\(image)")
+                .onReceive(timer) { _ in
+                    if image == 20 {
+                        image = 0
+                    } else {
+                        image += 1
+                    }
+                }
+        }
+    }
+}
+
+struct LoadingViewV2_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingViewV2()
+            .preferredColorScheme(.dark)
+    }
+}
