27927b37644cf6b9e5f1c5290d68dea6694bcaad
brien84 <houseinblues@gmail.com>
2022-12-26 00:45:05 +0200

    Implement `MovieClientErrorView`

--- Cinema/Main/MovieClientErrorView.swift
+++ Cinema/Main/MovieClientErrorView.swift
@@ -30,12 +30,57 @@ struct MovieClientErrorView: View {
     var body: some View {
         ZStack {
             Color.primaryBackground
+                .ignoresSafeArea()
 
-            VStack(spacing: .zero) {
-                Text("Error!")
+            GeometryReader { proxy in
+                let midPoint = CGPoint(
+                    x: proxy.frame(in: .local).midX,
+                    y: proxy.frame(in: .local).midY
+                )
+
+                Text(text)
+                    .font(.title.bold())
+                    .foregroundColor(.primaryElement)
+                    .multilineTextAlignment(.center)
+                    .position(midPoint)
+                    .offset(y: .textYOffset)
+
+                Image("empty")
+                    .frame(maxWidth: .infinity)
+                    .position(midPoint)
+                    .onTapGesture {
+                        action()
+                    }
+
+                Button {
+                    action()
+                } label: {
+                    Text(buttonText)
+                        .font(Font.footnote.weight(.semibold))
+                        .foregroundColor(.secondaryElement)
+                        .padding(.buttonPadding)
+                        .background(
+                            RoundedRectangle(cornerRadius: .buttonCornerRadius)
+                                .strokeBorder(Color.secondaryElement, lineWidth: .buttonLineWidth)
+                                .background(
+                                    RoundedRectangle(cornerRadius: .buttonCornerRadius).fill(Color.primaryBackground)
+                                )
+                        )
+                }
+                .position(midPoint)
+                .offset(y: .buttonYOffset)
             }
         }
     }
+}
+
+extension CGFloat {
+    static let textYOffset: CGFloat = -88
+    static let buttonYOffset: CGFloat = 104
+
+    static let buttonCornerRadius: CGFloat = 5
+    static let buttonLineWidth: CGFloat = 0.5
+    static let buttonPadding: CGFloat = 8
 }
 
 struct MovieClientErrorView_Previews: PreviewProvider {
