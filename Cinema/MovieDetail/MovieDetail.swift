bd9c0bdae8dc64fbfb0b48991bfa83f810d9a227
brien84 <houseinblues@gmail.com>
2022-12-29 14:31:20 +0200

    Create `MovieDetail` interface

---
+++ Cinema/MovieDetail/MovieDetail.swift
@@ -0,0 +1,31 @@
+//
+//  MovieDetail.swift
+//  Cinema
+//
+//  Created by Marius on 2022-12-29.
+//  Copyright © 2022 Marius. All rights reserved.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+struct MovieDetail: ReducerProtocol {
+
+    struct State: Equatable {
+
+    }
+
+    enum Action: Equatable {
+        case none
+    }
+
+    var body: some ReducerProtocol<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .none:
+                return .none
+            }
+        }
+    }
+
+}
