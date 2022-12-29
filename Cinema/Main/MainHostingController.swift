5b7d80771a33bd7e820b6d410b8d3c026acc5c29
brien84 <houseinblues@gmail.com>
2022-12-29 20:05:33 +0200

    Implement navigation to `MovieDetailHostingController`

--- Cinema/Main/MainHostingController.swift
+++ Cinema/Main/MainHostingController.swift
@@ -10,8 +10,6 @@ import Combine
 import ComposableArchitecture
 import SwiftUI
 
-private let navigationIdentifier = "showMovieViewController"
-
 final class MainHostingController: UIHostingController<MainView> {
     private var cancellables: Set<AnyCancellable> = []
     private let viewStore: ViewStoreOf<Main>
@@ -29,26 +27,33 @@ final class MainHostingController: UIHostingController<MainView> {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        navigationController?.navigationBar.setBackgroundImage(color: .primaryBackground)
         view.backgroundColor = .primaryBackground
 
         viewStore.publisher.selectedMovie
             .sink { movie in
                 if let movie {
-                    self.performSegue(withIdentifier: navigationIdentifier, sender: movie)
+                    let store = Store(initialState: MovieDetail.State(movie: movie, showing: nil), reducer: MovieDetail())
+                    let vc = MovieDetailHostingController(store: store)
+                    self.navigationController?.pushViewController(vc, animated: true)
                 }
             }.store(in: &self.cancellables)
 
         viewStore.publisher.selectedShowing
             .sink { showing in
-                if let showing {
-                    self.performSegue(withIdentifier: navigationIdentifier, sender: showing)
+                if let showing, let movie = showing.parentMovie {
+                    let store = Store(initialState: MovieDetail.State(movie: movie, showing: showing), reducer: MovieDetail())
+                    let vc = MovieDetailHostingController(store: store)
+                    self.navigationController?.pushViewController(vc, animated: true)
                 }
             }.store(in: &self.cancellables)
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+
+        DispatchQueue.main.async { [self] in
+            navigationController?.setNavigationBarHidden(true, animated: false)
+        }
 
         if viewStore.selectedMovie != nil || viewStore.selectedShowing != nil {
             viewStore.send(.deselect)
@@ -58,23 +63,8 @@ final class MainHostingController: UIHostingController<MainView> {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
 
-        UIView.animate(withDuration: .stdAnimation / 2) { [self] in
+        DispatchQueue.main.async { [self] in
             navigationController?.setNavigationBarHidden(true, animated: false)
-        }
-    }
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == navigationIdentifier {
-            guard let vc = segue.destination as? MovieViewController else { return }
-
-            if let movie = sender as? Movie {
-                vc.movie = movie
-            } else if let showing = sender as? Showing {
-                vc.movie = showing.parentMovie
-                vc.showing = showing
-            } else {
-                return
-            }
         }
     }
 }
