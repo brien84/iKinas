f8c8a9e4888e10a68dec6064dba1c9b40930a425
brien84 <houseinblues@gmail.com>
2022-12-31 12:59:00 +0200

    Create `DetailShowings` interface

---
+++ Cinema/DetailShowings/DetailShowings.swift
@@ -0,0 +1,31 @@
+//
+//  DetailShowings.swift
+//  Cinema
+//
+//  Created by Marius on 2022-12-30.
+//  Copyright © 2022 Marius. All rights reserved.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+struct DetailShowings: ReducerProtocol {
+
+    struct State: Equatable {
+
+    }
+
+    enum Action: Equatable {
+        case none
+    }
+
+    var body: some ReducerProtocol<State, Action> {
+        Reduce { _, action in
+            switch action {
+            case .none:
+                return .none
+            }
+        }
+    }
+
+}
