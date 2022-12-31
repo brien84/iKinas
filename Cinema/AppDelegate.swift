899d5b621103b3d2918bb8013c23a8e001494e12
brien84 <houseinblues@gmail.com>
2022-12-31 13:02:27 +0200

    Remove redundant code from `application(_:, didFinishLaunchingWithOptions:)` method

--- Cinema/AppDelegate.swift
+++ Cinema/AppDelegate.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2019 Marius. All rights reserved.
 //
 
-import ComposableArchitecture
 import SwiftUI
 
 @UIApplicationMain
@@ -15,16 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
-        let navController = window?.rootViewController as? UINavigationController
-
-        navController?.navigationBar.isHidden = true
-
-        // Opens `SettingsView` if the app is started for the first time or if UI tests commence.
-        #warning("Review this!")
-        if UserDefaults.standard.isFirstLaunch() || CommandLine.arguments.contains("ui-testing") {
-
-        }
 
         return true
     }
