0eaa6fa43782b97ef8869f22a23faab0edaa1553
brien84 <houseinblues@gmail.com>
2023-01-01 18:19:44 +0200

    Rework `Section` `ForEach` subview appearance

--- Cinema/DetailShowings/DetailShowingsView.swift
+++ Cinema/DetailShowings/DetailShowingsView.swift
@@ -12,36 +12,54 @@ import SwiftUI
 struct DetailShowingsView: View {
     let store: StoreOf<DetailShowings>
 
-    private let columns = [GridItem(.flexible()), GridItem(.flexible())]
-
     var body: some View {
         WithViewStore(store) { viewStore in
-            VStack(spacing: 16) {
-                Text("Seansai")
-                    .font(.title.bold())
-                    .foregroundColor(.primaryElement)
-                    .frame(maxWidth: .infinity, alignment: .leading)
+            VStack(spacing: Self.verticalSpacing) {
+                SectionLabel(text: "Seansai")
 
                 ForEach(viewStore.sections) { section in
-                    Text(section.date.asString(.monthAndDay))
-                        .font(.title3.weight(.semibold))
-                        .foregroundColor(viewStore.selectedSection == section ? .tertiaryElement : .secondaryElement)
-                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Button {
+                        viewStore.send(.didSelectSection(section), animation: .easeInOut)
+                    } label: {
+                        Text(section.date.sectionFormat)
+                            .font(.title3.weight(.semibold))
+                            .foregroundColor(viewStore.selectedSection == section ? .tertiaryElement : .secondaryElement)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
                 }
+                .transition(.verticalScaleAndOpacity)
             }
             .padding()
         }
     }
 }
 
+private extension Date {
+    var sectionFormat: String {
+        if Calendar.current.isDateInToday(self) { return "Šiandien" }
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "lt")
+        dateFormatter.dateFormat = "EEEE, MMMM d"
+        return dateFormatter.string(from: self)
+    }
+}
+
+// MARK: - Constants
+
+private extension DetailShowingsView {
+    static let verticalSpacing: CGFloat = 16
+}
+
 // MARK: - Previews
 
 struct MovieShowingsView_Previews: PreviewProvider {
     static let showings = [
-        Showing(date: Date(timeIntervalSinceNow: 3600)),
-        Showing(date: Date(timeIntervalSinceNow: 9000)),
-        Showing(date: Date(timeIntervalSinceNow: 90000)),
-        Showing(date: Date(timeIntervalSinceNow: 180000))
+        Showing(date: Date(timeIntervalSinceNow: 60)),
+        Showing(date: Date(timeIntervalSinceNow: 60)),
+        Showing(date: Date(timeIntervalSinceNow: 86400)),
+        Showing(date: Date(timeIntervalSinceNow: 2 * 86400)),
+        Showing(date: Date(timeIntervalSinceNow: 3 * 86400)),
+        Showing(date: Date(timeIntervalSinceNow: 4 * 86400))
     ]
 
     static let store = Store(initialState: DetailShowings.State(movie: Movie(showings: showings)), reducer: DetailShowings())
