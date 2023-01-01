7facf23cfa2f29a1aaa484d4bfa0e9e349d482d8
brien84 <houseinblues@gmail.com>
2023-01-01 20:35:03 +0200

    Add `JustifiedTextView`

--- Cinema/MovieDetail/BodyView.swift
+++ Cinema/MovieDetail/BodyView.swift
@@ -17,15 +17,102 @@ struct BodyView: View {
             ZStack {
                 Color.primaryBackground
 
-                VStack(spacing: 0) {
+                VStack(spacing: .zero) {
                     GenresView(genres: viewStore.movie.genres)
                         .frame(maxWidth: .infinity, alignment: .leading)
                         .padding(.horizontal)
-                        .padding(.vertical, 8)
+                        .padding(.vertical, Self.verticalPadding)
 
                     Divider()
                         .padding(.horizontal)
+                        .padding(.bottom, Self.verticalPadding)
+
+                    JustifiedTextView(text: viewStore.movie.plot)
+                        .padding(.horizontal, Self.horizontalPadding)
+
+                    Divider()
+                        .padding(.horizontal)
+                        .padding(.vertical, Self.verticalPadding)
                 }
+            }
+        }
+    }
+}
+
+private struct GenresView: View {
+    let genres: [String]
+
+    var body: some View {
+        HStack {
+            ForEach(genres, id: \.self) { genre in
+                ZStack {
+                    RoundedRectangle(cornerRadius: Self.cornerRadius)
+                        .stroke()
+
+                    Text(genre)
+                        .font(.caption2)
+                        .padding(.horizontal, Self.horizontalPadding)
+                        .padding(.vertical, Self.verticalPadding)
+                }
+                .fixedSize(horizontal: true, vertical: true)
+            }
+        }
+    }
+}
+
+private struct JustifiedTextView: UIViewRepresentable {
+    let text: String
+
+    func makeUIView(context: UIViewRepresentableContext<Self>) -> UILabel {
+        let label = UILabel()
+
+        label.numberOfLines = 0
+        label.lineBreakMode = .byTruncatingTail
+        label.textAlignment = .justified
+        label.textColor = .primaryElement
+
+        return label
+    }
+
+    func updateUIView(_ uiView: UILabel, context: UIViewRepresentableContext<Self>) {
+        uiView.text = text
+        uiView.preferredMaxLayoutWidth = UIScreen.main.bounds.width - BodyView.horizontalPadding * 2
+    }
+}
+
+// MARK: - Constants
+
+private extension BodyView {
+    static let horizontalPadding: CGFloat = 16
+    static let verticalPadding: CGFloat = 8
+}
+
+private extension GenresView {
+    static let cornerRadius: CGFloat = 4
+    static let horizontalPadding: CGFloat = 8
+    static let verticalPadding: CGFloat = 4
+}
+
+// MARK: - Previews
+
+struct BodyView_Previews: PreviewProvider {
+    static let showings = [
+        Showing(date: Date(timeIntervalSinceNow: 60)),
+        Showing(date: Date(timeIntervalSinceNow: 86400)),
+        Showing(date: Date(timeIntervalSinceNow: 2 * 86400))
+    ]
+
+    static let movie = Movie(showings: showings)
+    static let store = Store(initialState: MovieDetail.State(movie: movie), reducer: MovieDetail())
+
+    static var previews: some View {
+        ZStack {
+            Color.primaryBackground
+                .ignoresSafeArea()
+
+            ScrollView {
+                BodyView(store: store)
+                    .preferredColorScheme(.dark)
             }
         }
     }
