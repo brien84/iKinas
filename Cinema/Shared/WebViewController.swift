b9222ae0812a7fae1e22102ff33f91bc2a37df5e
brien84 <houseinblues@gmail.com>
2023-01-06 23:51:06 +0200

    Refactor `WebViewController` to remove dependency on storyboard

--- Cinema/Shared/WebViewController.swift
+++ Cinema/Shared/WebViewController.swift
@@ -10,22 +10,57 @@ import UIKit
 import WebKit
 
 final class WebViewController: UIViewController, WKUIDelegate {
-    var url: URL?
 
-    @IBOutlet private weak var webView: WKWebView!
+    private let request: URLRequest
+
+    private lazy var webView: WKWebView = {
+        let config = WKWebViewConfiguration()
+        let webView = WKWebView(frame: .zero, configuration: config)
+        webView.uiDelegate = self
+        return webView
+    }()
+
+    private lazy var rightButton: UIBarButtonItem = {
+        let button = UIBarButtonItem(
+            title: "Done",
+            primaryAction: UIAction { [weak self] _ in
+                self?.navigationController?.popViewController(animated: true)
+            }
+        )
+
+        return button
+    }()
+
+    init(url: URL) {
+        self.request = URLRequest(url: url)
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func loadView() {
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        webView.uiDelegate = self
+        view.backgroundColor = .primaryBackground
+        webView.isOpaque = false
 
-        if let url = url {
-            let request = URLRequest(url: url)
-            webView.load(request)
-        }
+        webView.load(request)
+
+        navigationController?.interactivePopGestureRecognizer?.isEnabled = false
     }
 
-    @IBAction private func doneButtonDidTap(_ sender: UIBarButtonItem) {
-        navigationController?.popViewController(animated: true)
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        navigationItem.hidesBackButton = true
+        navigationItem.rightBarButtonItem = rightButton
+
+        navigationController?.navigationBar.setBackgroundImage(color: .primaryBackground, alpha: 1)
     }
 }
