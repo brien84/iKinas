aca59f107e3ce63bc32df13b8ddee08c99992b70
brien84 <houseinblues@gmail.com>
2023-01-10 09:42:53 +0200

    Create `ShowingDetailErrorView`

---
+++ Cinema/ShowingDetail/ShowingDetailErrorView.swift
@@ -0,0 +1,61 @@
+//
+//  ShowingDetailErrorView.swift
+//  Cinema
+//
+//  Created by Marius on 2023-01-10.
+//  Copyright © 2023 Marius. All rights reserved.
+//
+
+import SwiftUI
+
+struct ShowingDetailErrorView: View {
+
+    var body: some View {
+        ZStack {
+            Color.primaryBackground
+                .ignoresSafeArea()
+
+            GeometryReader { proxy in
+                let midPoint = CGPoint(
+                    x: proxy.frame(in: .local).midX,
+                    y: proxy.frame(in: .local).midY
+                )
+
+                Text("seansų nėra")
+                    .font(.largeTitle.bold())
+                    .foregroundColor(.primaryElement)
+                    .position(midPoint)
+                    .offset(y: Self.titleYOffset)
+
+                Image("empty")
+                    .frame(maxWidth: .infinity)
+                    .position(midPoint)
+                    .offset(y: Self.imageYOffset)
+
+                Text("pasirinkite kitą filmą")
+                    .font(.body)
+                    .foregroundColor(.secondaryElement)
+                    .position(midPoint)
+                    .offset(y: Self.subtitleYOffset)
+            }
+        }
+    }
+
+}
+
+// MARK: - Constants
+
+extension ShowingDetailErrorView {
+    static let imageYOffset: CGFloat = -16
+    static let subtitleYOffset: CGFloat = 80
+    static let titleYOffset: CGFloat = -88
+}
+
+// MARK: - Previews
+
+struct ShowingDetailErrorView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShowingDetailErrorView()
+            .preferredColorScheme(.dark)
+    }
+}
