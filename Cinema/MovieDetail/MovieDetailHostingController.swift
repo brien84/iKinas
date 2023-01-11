de034bafa34ea13d9a79d7943a347bdf15af4ead
brien84 <houseinblues@gmail.com>
2023-01-11 17:50:51 +0200

    Move `setBackgroundImage(color:, alpha:)` `UINavigationBar` extension method to `MovieDetailHostingController`

--- Cinema/MovieDetail/MovieDetailHostingController.swift
+++ Cinema/MovieDetail/MovieDetailHostingController.swift
@@ -170,6 +170,24 @@ private extension UIBarButtonItem {
     }
 }
 
+extension UINavigationBar {
+    func setBackgroundImage(color: UIColor, alpha: CGFloat = 1.0) {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithTransparentBackground()
+
+        // If `alpha` is equal or higher than 1.0 `UINavigationBar`
+        // will apply a system-defined alpha value.
+        let alpha = alpha >= 0.99 ? 0.99 : alpha
+        let color = color.withAlphaComponent(alpha)
+
+        // `UINavigationBar` will size the image to fill.
+        appearance.backgroundImage = color.image(size: CGSize(width: 1, height: 1))
+
+        self.standardAppearance = appearance
+        self.scrollEdgeAppearance = appearance
+    }
+}
+
 // MARK: - Constants
 
 private extension MovieDetailHostingController {
