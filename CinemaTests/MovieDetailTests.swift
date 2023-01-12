b6e697bb81ba13d570b07bfd59f5fe35ebc404a3
brien84 <houseinblues@gmail.com>
2023-01-13 00:18:47 +0200

    Create `MovieDetailTests`

---
+++ CinemaTests/MovieDetailTests.swift
@@ -0,0 +1,20 @@
+//
+//  MovieDetailTests.swift
+//  CinemaTests
+//
+//  Created by Marius on 2023-01-13.
+//  Copyright © 2023 Marius. All rights reserved.
+//
+
+import ComposableArchitecture
+import XCTest
+@testable import iKinas
+
+@MainActor
+final class MovieDetailTests: XCTestCase {
+
+    func test() async {
+        XCTAssertTrue(true)
+    }
+
+}
