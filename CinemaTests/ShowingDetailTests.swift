24fb51546fa67e70bcb8614ec85c05610a4760ea
brien84 <houseinblues@gmail.com>
2023-01-13 00:18:04 +0200

    Create `ShowingDetailTests`

---
+++ CinemaTests/ShowingDetailTests.swift
@@ -0,0 +1,20 @@
+//
+//  ShowingDetailTests.swift
+//  CinemaTests
+//
+//  Created by Marius on 2023-01-13.
+//  Copyright © 2023 Marius. All rights reserved.
+//
+
+import ComposableArchitecture
+import XCTest
+@testable import iKinas
+
+@MainActor
+final class ShowingDetailTests: XCTestCase {
+
+    func test() async {
+        XCTAssertTrue(true)
+    }
+
+}
