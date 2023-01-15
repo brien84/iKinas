f01821bb7d3d4a179cf2910be8ffeea410027b62
brien84 <houseinblues@gmail.com>
2023-01-15 21:15:17 +0200

    Test `Schedule`

--- CinemaTests/ScheduleTests.swift
+++ CinemaTests/ScheduleTests.swift
@@ -13,8 +13,39 @@ import XCTest
 @MainActor
 final class ScheduleTests: XCTestCase {
 
-    func test() async {
-        XCTAssertTrue(true)
+    func testApplyingDatasource() async {
+        let date = Date()
+        let showings = [Showing()]
+        let movies = [Movie(showings: showings)]
+
+        let datasource = Schedule.Datasource(
+            date: date,
+            movies: movies
+        )
+
+        let store = TestStore(
+            initialState: Schedule.State(datasource: datasource),
+            reducer: Schedule()
+        )
+
+        store.dependencies.uuid = .incrementing
+
+        await store.send(.applyDatasource) {
+            $0.selectedDate = date
+            $0.movieItems = [MovieItem.State(id: UUID(uuidString: "00000000-0000-0000-0000-000000000000")!, movie: movies[0])]
+            $0.showingItems = [ShowingItem.State(id: UUID(uuidString: "00000000-0000-0000-0000-000000000001")!, showing: showings[0])]
+        }
+    }
+
+    func testEndingTransition() async {
+        let store = TestStore(
+            initialState: Schedule.State(didUpdateDatasource: true),
+            reducer: Schedule()
+        )
+
+        await store.send(.transitionDidEnd) {
+            $0.didUpdateDatasource = false
+        }
     }
 
 }
