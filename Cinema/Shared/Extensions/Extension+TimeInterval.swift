1abcf3a38da9d61c413e73b0cedb9cc64226cd20
brien84 <houseinblues@gmail.com>
2023-07-20 22:57:05 +0300

    Add `TimeInterval` constants

---
+++ Cinema/Shared/Extensions/Extension+TimeInterval.swift
@@ -0,0 +1,14 @@
+//
+//  Extension+TimeInterval.swift
+//  Cinema
+//
+//  Created by Marius on 2023-07-19.
+//  Copyright © 2023 Marius. All rights reserved.
+//
+
+import Foundation
+
+extension TimeInterval {
+    static let fullDay: TimeInterval = 86400
+    static let hour: TimeInterval = 3600
+}
