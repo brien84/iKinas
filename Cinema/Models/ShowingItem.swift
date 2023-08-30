11c9c7f6183ea2d16c34b88c43d859ebdb7545e8
brien84 <houseinblues@gmail.com>
2023-08-30 23:01:52 +0300

    Create `ShowingItem` interface

---
+++ Cinema/Models/ShowingItem.swift
@@ -0,0 +1,43 @@
+//
+//  ShowingItem.swift
+//  Cinema
+//
+//  Created by Marius on 2023-08-30.
+//  Copyright © 2023 Marius. All rights reserved.
+//
+
+import ComposableArchitecture
+import Foundation
+
+struct ShowingItem: ReducerProtocol {
+    struct State: Equatable, Identifiable {
+        var id: UUID { showing.id }
+        let showing: Showing
+        var networkImage: NetworkImage.State
+
+        init?(showing: Showing) {
+            self.showing = showing
+            self.networkImage = NetworkImage.State(url: showing.posterURL)
+        }
+    }
+
+    enum Action: Equatable {
+        case didSelect
+        case networkImage(NetworkImage.Action)
+    }
+
+    var body: some ReducerProtocol<State, Action> {
+        Scope(state: \.networkImage, action: /Action.networkImage) {
+            NetworkImage()
+        }
+
+        Reduce { _, action in
+            switch action {
+            case .didSelect:
+                return .none
+            case .networkImage:
+                return .none
+            }
+        }
+    }
+}
