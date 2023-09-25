ba29330ba1abbbd71d2a189aedf04dbbd6979b78
brien84 <houseinblues@gmail.com>
2023-09-25 15:00:19 +0300

    Add `app` `Locale` extension property for uniform localization settings

---
+++ Cinema/Shared/Extensions/Extension+Locale.swift
@@ -0,0 +1,13 @@
+//
+//  Extension+Locale.swift
+//  Cinema
+//
+//  Created by Marius on 2023-09-25.
+//  Copyright © 2023 Marius. All rights reserved.
+//
+
+import Foundation
+
+extension Locale {
+    static let app = Locale(identifier: "lt")
+}
