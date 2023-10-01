77e49f6e92db4ecba0cafe0fc8ff331bbdc7de85
brien84 <houseinblues@gmail.com>
2023-10-01 18:07:43 +0300

    Create `GreetingLabel` view

---
+++ Cinema/Shared/Views/TextLabels/GreetingLabel.swift
@@ -0,0 +1,39 @@
+//
+//  GreetingLabel.swift
+//  Cinema
+//
+//  Created by Marius on 2023-10-01.
+//  Copyright © 2023 Marius. All rights reserved.
+//
+
+import SwiftUI
+
+struct GreetingLabel: View {
+    private var greeting: String {
+        switch Calendar.current.component(.hour, from: Date()) {
+        case 5...11:
+            return "Labas rytas!"
+        case 12...17:
+            return "Laba diena!"
+        case 0...4, 18...23:
+            return "Labas vakaras!"
+        default:
+            return "Sveiki!"
+        }
+    }
+
+    var body: some View {
+        VStack {
+            Text(greeting)
+                .font(.largeTitle.bold())
+                .foregroundColor(.primaryElement)
+        }
+    }
+}
+
+struct GreetingLabel_Previews: PreviewProvider {
+    static var previews: some View {
+        GreetingLabel()
+            .preferredColorScheme(.dark)
+    }
+}
