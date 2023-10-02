c7535754aff63cf12105b231a42e3aa804d29098
brien84 <houseinblues@gmail.com>
2023-10-02 08:34:09 +0300

    Create `UpcomingListView`

---
+++ Cinema/HomeFeed/Subviews/UpcomingListView.swift
@@ -0,0 +1,23 @@
+//
+//  UpcomingListView.swift
+//  Cinema
+//
+//  Created by Marius on 2023-10-02.
+//  Copyright © 2023 Marius. All rights reserved.
+//
+
+import SwiftUI
+
+struct UpcomingListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - Previews
+
+struct UpcomingListView_Previews: PreviewProvider {
+    static var previews: some View {
+        UpcomingListView()
+    }
+}
