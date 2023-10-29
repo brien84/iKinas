ae9bed916145e33d7dbe31c77ea0a6a0d3af5384
brien84 <houseinblues@gmail.com>
2023-10-29 19:43:49 +0200

    Implement `FeaturedView`

--- Cinema/HomeFeed/Subviews/FeaturedListView.swift
+++ Cinema/HomeFeed/Subviews/FeaturedListView.swift
@@ -30,11 +30,44 @@ private struct FeaturedView: View {
     var body: some View {
         WithViewStore(store) { viewStore in
             ShrinkOnPressView {
-                NetworkImageView(store: store.scope(
-                    state: \.networkImage,
-                    action: Featured.Action.networkImage
-                ))
-                .aspectRatio(contentMode: .fit)
+                ZStack {
+                    NetworkImageView(store: store.scope(
+                        state: \.networkImage,
+                        action: Featured.Action.networkImage
+                    ))
+                    .aspectRatio(contentMode: .fit)
+
+                    VStack(alignment: .leading, spacing: Self.verticalSpacing) {
+                        let isTitleEqualToOriginalTitle = viewStore.title == viewStore.originalTitle
+
+                        Text(viewStore.label.uppercased())
+                            .font(.subheadline.bold())
+                            .foregroundColor(.tertiaryElement)
+                            .lineLimit(1)
+
+                        Text(viewStore.title)
+                            .font(.title.bold())
+                            .lineLimit(isTitleEqualToOriginalTitle ? 3 : 2)
+
+                        if !isTitleEqualToOriginalTitle {
+                            Text(viewStore.originalTitle)
+                                .font(.subheadline)
+                                .foregroundColor(.secondaryElement)
+                                .lineLimit(1)
+                        }
+                    }
+                    .padding()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .background(Color.secondaryBackground)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomLeading)
+
+                    RoundedRectangle(cornerRadius: Self.cornerRadius)
+                        .strokeBorder(Color.primaryElement, lineWidth: Self.lineWidth)
+                }
+                .aspectRatio(1, contentMode: .fit)
+                .contentShape(RoundedRectangle(cornerRadius: Self.cornerRadius))
+                .cornerRadius(Self.cornerRadius)
+                .padding(.horizontal)
             }
         }
     }
@@ -44,4 +77,10 @@ private struct FeaturedView: View {
 
 private extension FeaturedListView {
     static let verticalSpacing: CGFloat = 16
+}
+
+private extension FeaturedView {
+    static let cornerRadius: CGFloat = 15
+    static let lineWidth: CGFloat = 3
+    static let verticalSpacing: CGFloat = 8
 }
