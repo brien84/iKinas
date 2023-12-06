784750168ce89899b9a6926d05672401e1dcc7e7
brien84 <houseinblues@gmail.com>
2023-12-06 18:18:16 +0200

    Update `ScheduleTests`

--- CinemaTests/ScheduleTests.swift
+++ CinemaTests/ScheduleTests.swift
@@ -13,38 +13,88 @@ import XCTest
 @MainActor
 final class ScheduleTests: XCTestCase {
 
-    func testApplyingDatasource() async {
-        let date = Date()
-        let showings = [Showing()]
-        let movies = [Movie(showings: showings)]
+    func testFilteringDatasource() async {
+        let datasource = [
+            Previews.createShowing(date: Date(timeIntervalSinceNow: .hour), title: "test0"),
+            Previews.createShowing(date: Date(timeIntervalSinceNow: .hour), title: "test0"),
+            Previews.createShowing(date: Date(timeIntervalSinceNow: .hour), title: "test1"),
+            Previews.createShowing(date: .distantFuture, title: "test2")
+        ].convertToIdentifiedArray()
 
-        let datasource = Schedule.Datasource(
-            date: date,
-            movies: movies
-        )
+        let state = Schedule.State(datasource: datasource)
+        let store = TestStore(initialState: state, reducer: Schedule())
 
-        let store = TestStore(
-            initialState: Schedule.State(datasource: datasource),
-            reducer: Schedule()
-        )
+        await store.send(.filterDatasource) {
+            var showings = [datasource[0], datasource[1], datasource[2]].convertToIdentifiedArray()
+            showings.sort(by: .date)
+            $0.showings = showings
 
-        store.dependencies.uuid = .incrementing
-
-        await store.send(.applyDatasource) {
-            $0.selectedDate = date
-            $0.movieItems = [MovieItem.State(id: UUID(uuidString: "00000000-0000-0000-0000-000000000000")!, movie: movies[0])]
-            $0.showingItems = [ShowingItem.State(id: UUID(uuidString: "00000000-0000-0000-0000-000000000001")!, showing: showings[0])]
+            var movies = [datasource[0], datasource[2]].convertToIdentifiedArray()
+            movies.sort(by: .title)
+            $0.movies = movies
         }
     }
 
-    func testEndingTransition() async {
-        let store = TestStore(
-            initialState: Schedule.State(didUpdateDatasource: true),
-            reducer: Schedule()
-        )
+    func testFilteringDatasourceWithEnabledTimeFilter() async {
+        let referenceDate = Date(timeIntervalSinceNow: .hour)
+        let datasource = [
+            Previews.createShowing(date: Date(timeInterval: .hour * 1, since: referenceDate), title: "test0"),
+            Previews.createShowing(date: Date(timeInterval: .hour * 2, since: referenceDate), title: "test0"),
+            Previews.createShowing(date: Date(timeInterval: .hour * 3, since: referenceDate), title: "test1"),
+            Previews.createShowing(date: Date(timeInterval: .hour * 4, since: referenceDate), title: "test2")
+        ].convertToIdentifiedArray()
 
-        await store.send(.transitionDidEnd) {
-            $0.didUpdateDatasource = false
+        let startTime = Date(timeInterval: .hour * 2, since: referenceDate)
+        let endTime = Date(timeInterval: .hour * 3 + 1, since: referenceDate)
+        let timeFilter = TimeFilter(startTime: startTime, endTime: endTime)
+        let state = Schedule.State(isTimeFiltering: true, timeFilter: timeFilter, datasource: datasource)
+        let store = TestStore(initialState: state, reducer: Schedule())
+
+        await store.send(.filterDatasource) {
+            var showings = [datasource[1], datasource[2]].convertToIdentifiedArray()
+            showings.sort(by: .date)
+            $0.showings = showings
+
+            var movies = [datasource[0], datasource[2]].convertToIdentifiedArray()
+            movies.sort(by: .title)
+            $0.movies = movies
+        }
+    }
+
+    func testTogglingTimeFiltering() async {
+        let state = Schedule.State()
+        let store = TestStore(initialState: state, reducer: Schedule())
+
+        await store.send(.toggleTimeFiltering) {
+            $0.isTimeFiltering = true
+        }
+
+        await store.send(.toggleTimeFiltering) {
+            $0.isTimeFiltering = false
+        }
+    }
+
+    func testUpdatingDatasourceWhenMovieImageIsFetched() async {
+        let datasource = [Previews.createShowing()].convertToIdentifiedArray()
+        let movie = datasource.first!
+        let state = Schedule.State(datasource: datasource, movies: [movie].convertToIdentifiedArray())
+        let store = TestStore(initialState: state, reducer: Schedule())
+
+        await store.send(.movie(id: movie.id, action: .networkImage(.imageClient(.success(UIImage()))))) {
+            $0.movies[id: movie.id]!.networkImage.image = UIImage()
+            $0.datasource[id: movie.id]?.networkImage = $0.movies[id: movie.id]!.networkImage
+        }
+    }
+
+    func testUpdatingDatasourceWhenShowingImageIsFetched() async {
+        let datasource = [Previews.createShowing()].convertToIdentifiedArray()
+        let showing = datasource.first!
+        let state = Schedule.State(datasource: datasource, showings: [showing].convertToIdentifiedArray())
+        let store = TestStore(initialState: state, reducer: Schedule())
+
+        await store.send(.showing(id: showing.id, action: .networkImage(.imageClient(.success(UIImage()))))) {
+            $0.showings[id: showing.id]!.networkImage.image = UIImage()
+            $0.datasource[id: showing.id]?.networkImage = $0.showings[id: showing.id]!.networkImage
         }
     }
 
